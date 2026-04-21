1f5fe571244a6ceac6794d76061361b50638a7fe
Marcimave <marcimavegithub@gmail.com>
2026-04-21 11:18:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-console.log("JS connecté !");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("formulaire");
-  const confirmation = document.getElementById("confirmation");
-
-  if (form) {
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      confirmation.textContent = "✅ Message envoyé !";
-    });
-  }
-});
